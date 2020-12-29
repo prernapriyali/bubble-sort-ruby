9de10daaef8c7a86ad5f297a5af1ab5fa4db6df4
Prerna <ppriyali0711@gmail.com>
2020-12-29 22:26:39 +0530

    rubocop changes

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,15 @@
 def bubble_sort(array)
-    length = array.size 
-    loop do 
-      swapped = false
-      (length - 1).times do |i|
-        if array[i] > array[i + 1]
-          array[i], array[i + 1] = array[i +1], array[i]
-          swapped = true
-        end
-      end
-      break if not swapped
+  items = array.length
+  array.each do
+    i = 0
+    count = 0
+    while i < items - 1
+      array[i], array[i + 1] = array[i + 1], array[i] if array[i] > array[i + 1]
+      i += 1
+      count += 1
     end
-  array
+    break if count.zero?
   end
-    array = [4,3,78,2,0,2,4,3,152,25,0,36,152,2,5,6,8,7,9,12,15,15]
-    p bubble_sort(array)
+end
+
+bubble_sort([3, 63, 9, 2, 0, 2])
