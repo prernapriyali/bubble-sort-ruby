259a26130a7ca0e0272dec5f27e6024a941a879b
Prerna <ppriyali0711@gmail.com>
2020-12-30 16:19:32 +0530

    bubble sort by method completed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,3 +13,12 @@ def bubble_sort(array)
 end
 
 bubble_sort([3, 63, 9, 2, 0, 2])
+def bubble_sort_by(arr)
+  (0..arr.length - 2).each do |i|
+    sort_it = yield(arr[i], arr[i + 1])
+    arr[i], arr[i + 1] = arr[i + 1], arr[i] if sort_it.positive?
+  end
+  arr
+end
+
+bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
