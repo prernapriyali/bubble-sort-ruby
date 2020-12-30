7d1c8a6b43903fa42e3b2d6ef4caaf9e4357ccc9
Prerna <ppriyali0711@gmail.com>
2020-12-30 16:55:04 +0530

    rubocop error changes

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -22,4 +22,4 @@ def bubble_sort_by(arr)
   arr
 end
 
-p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
+bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
