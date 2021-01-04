211ea716051930ce121ac11b2af935f9ba565e7c
Prerna <ppriyali0711@gmail.com>
2021-01-04 13:28:36 +0530

    bubble sort by completed

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -13,13 +13,3 @@ def bubble_sort(array)
 end
 
 p bubble_sort([3, 63, 9, 2, 0, 2])
-
-def bubble_sort_by(arr)
-  (0..arr.length - 2).each do |i|
-    sort_it = yield(arr[i], arr[i + 1])
-    arr[i], arr[i + 1] = arr[i + 1], arr[i] if sort_it.positive?
-  end
-  arr
-end
-
-p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
---
+++ bubble-sort/bubble_sort_by.rb
@@ -0,0 +1,18 @@
+def bubble_sort_by(array)
+  (array.length).times do
+    swap = 0
+    (array.length - 1).times do |i|
+      if yield(array[i], array[i+1]) > 0
+        array[i], array[i+1] = array[i+1], array[i]
+        swap += 1
+      end
+    end
+    break array if swap == 0
+  end
+end
+
+sorted_array = bubble_sort_by(["pal","hello","h","hola"]) do |left,right|
+  left.length - right.length
+end
+
+puts sorted_array
